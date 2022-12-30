951dab6f80e744156a3d4795d5635bd170dec95d
Nauzet <nauuuni@gmail.com>
2022-12-30 18:09:01 +0000

    Commit MoneyCalculator v2

--- src/moneycalculator/view/Dialog.java
+++
@@ -1,9 +0,0 @@
-package moneycalculator.view;
-
-import moneycalculator.model.Currency;
-import moneycalculator.model.Money;
-
-public interface Dialog {
-    public Money getMoney();
-    public Currency getCurrencyTo();
-}
